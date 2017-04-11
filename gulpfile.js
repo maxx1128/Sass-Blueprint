a387ae99a1157622f907c5250eafc8c7e2997df8
Max Antonucci <maxx1128@gmail.com>
2017-04-11 16:05:36 -0400

    Updated file structure, added linting, browser sync, and watching

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
-var
-    gulp    = require('gulp'),
-    notify  = require('gulp-notify'),
-    plumber = require('gulp-plumber'),
-    rename  = require('gulp-rename'),
-    sass    = require('gulp-sass')
+var gulp        = require('gulp'),
+    notify      = require('gulp-notify'),
+    plumber     = require('gulp-plumber'),
+    rename      = require('gulp-rename'),
+    sass        = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    runSequence = require('run-sequence'),
+    scsslint    = require('gulp-scss-lint')
 ;  
 
 // Find errors!
@@ -24,12 +26,29 @@ function customPlumber(errTitle) {
     });
 }
 
+// Browser Sync settings and config
+var bs_reload = {
+    stream: true
+};
+
+gulp.task('browserSync', function() {
+    var Settings = {
+        files: ['test/**'],
+        port: 4000,
+        server: { baseDir: 'test' },
+        reload: ({ stream: true}),
+        notify: false
+    };
+
+    browserSync(Settings)
+});
+
 
 // Converts the Sass partials into a single CSS file
 gulp.task('sass', function () {
     
     // Sass and styling variables
-    var sassInput = 'main.scss';
+    var sassInput = '_sass/main.scss';
     var sassOptions = { 
         outputStyle: 'expanded'
     };
@@ -45,6 +64,31 @@ gulp.task('sass', function () {
     // Sends the Sass file to either the app or dist folder
     .pipe(gulp.dest('test'))
     .pipe(notify({ message: 'Sass Processed!', onLast: true }))
+    .pipe(browserSync.reload(bs_reload))
 });
 
-gulp.task('default', ['sass']);
+// For linting the Sass files
+gulp.task('scss-lint', function() {
+  return gulp.src([
+      '_sass/**/**/**/*.scss',
+      '!_sass/_generic/_normalize.scss',
+      '!_sass/_tools/_synapse.scss'
+    ])
+    .pipe(scsslint({
+      'maxBuffer': 9999999999999999999999999999999999999999,
+      'config': '.css-guidelines.yml'
+    }))
+});
+
+// Task to watch the things!
+gulp.task('watch', function(){
+  gulp.watch('_sass/**/**/*.scss', ['sass']);
+});
+
+gulp.task('default', function(callback) {
+  runSequence(
+    'sass',
+    ['browserSync', 'watch'],
+    callback
+  )
+});
